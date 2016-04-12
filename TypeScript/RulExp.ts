d80a7d36225c1704701a047ae6834fca299ded99
Vincent Van Ingen <hi@abitvin.net>
2016-04-12 02:28:13 +0200

    Implemented first range instruction.

--- TypeScript/RulExp.ts
+++ TypeScript/RulExp.ts
@@ -19,11 +19,28 @@ namespace Abitvin
     // one:         <rule>
     
     
+    const enum RangeType
+    {
+        NoRangeType,
+        AtLeast,
+        //AtMost,
+        //Between,
+        //Exact,
+        //NoneOrMany
+    }
     
+    interface IParseContext<TB, TM>
+    {
+        arg1: number;   // Range argument 1
+        arg2: number;   // Range argument 2
+        arg3: string;   // Text or id
+        rangeType: RangeType;
+        rule: Rule<TB, TM>;
+    }
     
     interface IEmpty {}
     
-    class R<TB, TM> extends Rule<Rule<TB, TM>, IEmpty> {}
+    class R<TB, TM> extends Rule<IParseContext<TB, TM>, IEmpty> {}
     
     interface IRule<TBranch, TMeta>
     {
@@ -39,6 +56,7 @@ namespace Abitvin
         
         constructor()
         {
+            const ranges = new R<TBranch, TMeta>();
             const statement = new R<TBranch, TMeta>();
             
             const char = new R<TBranch, TMeta>().between("a", "z");
@@ -52,28 +70,104 @@ namespace Abitvin
             //const eof = new R().literal("EOF");
             
             // Literal
-            const literal = new R<TBranch, TMeta>((b, l) => [new Rule<TBranch, TMeta>().literal(l)]).atLeast(1, Char);
+            const literalTextFn = (b, l) => [{
+                arg1: null, 
+                arg2: null,
+                arg3: l, 
+                rangeType: RangeType.NoRangeType, 
+                rule: null 
+            }];
+            
+            const literalText = new R<TBranch, TMeta>(literalTextFn).atLeast(1, Char);
+            
+            const literalFn = (b, l) =>
+            {
+                const text = b[0].arg3;
+                let rule = new Rule<TBranch, TMeta>().literal(text);
+                
+                if (b.length === 2) 
+                    switch(b[1].rangeType)
+                    {
+                        case RangeType.AtLeast:
+                        {
+                            rule = new Rule<TBranch, TMeta>().atLeast(b[1].arg1, rule);
+                            break;
+                        }
+                        
+                        default:
+                            throw new Error("Not implemented.");
+                    }
+                    
+                return [{ 
+                    arg1: null,
+                    arg2: null,
+                    arg3: null,
+                    rangeType: RangeType.NoRangeType,
+                    rule: rule 
+                }];
+            };
+            
+            const literal = new R<TBranch, TMeta>(literalFn).one(literalText).maybe(ranges);
             
             // One rule
+            const ruleNameFn = (b, l) => [{
+                arg1: null, 
+                arg2: null,
+                arg3: l, 
+                rangeType: RangeType.NoRangeType, 
+                rule: null 
+            }];
+            
+            const ruleName = new R<TBranch, TMeta>(ruleNameFn).atLeast(1, Char);
+            
             const ruleFn = (b, l) =>
             {
-                const id = l.slice(1,-1);
+                const id = b[0].arg3;
                 const r = this._rulexps[id];
                 
                 if (r == null)
                     throw new Error(`Rule "${id}" not found.`);
+                
+                let rule = null;
+                
+                if (b.length === 1)
+                {
+                    rule = new Rule<TBranch, TMeta>().one(r.rule); 
+                }
+                else switch(b[1].rangeType)
+                {
+                    case RangeType.AtLeast:
+                    {
+                        rule = new Rule<TBranch, TMeta>().atLeast(b[1].arg1, r.rule);
+                        break;
+                    }
                     
-                return [new Rule<TBranch, TMeta>().one(r.rule)];
+                    default:
+                        throw new Error("Not implemented.");
+                }
+                    
+                return [{ 
+                    arg1: null,
+                    arg2: null,
+                    arg3: null,
+                    rangeType: RangeType.NoRangeType,
+                    rule: rule 
+                }];
             };
-            
-            //const ranges = new R<TBranch, TMeta>(); 
-            
-            // TODO add ranges (*, +, [n,m]) after this rule to fix the atLeastOne stack overflow.
-            const rule = new R<TBranch, TMeta>(ruleFn).literal("<").atLeast(1, Char).literal(">"); //.maybe(ranges);
+             
+            const rule = new R<TBranch, TMeta>(ruleFn).literal("<").one(ruleName).literal(">").maybe(ranges);
             
             // At least one
-            //const atLeastOneFn = (b, l) => [new Rule<TBranch, TMeta>().atLeast(1, b[0])];
-            //const atLeastOne = new R<TBranch, TMeta>(atLeastOneFn).one(rule /* TODO if we use `statement` here we get a stack overflow (which is logical). */).literal("+");
+            const atLeastOneFn = (b, l) => [{
+                arg1: 1,
+                arg2: null,
+                arg3: null,
+                rangeType: RangeType.AtLeast,
+                rule: null
+            }];
+            
+            const atLeastOne = new R<TBranch, TMeta>(atLeastOneFn).literal("+");
+            
             //const atMost = new R().one(rule).literal("{,").atLeast(1, digit).literal("}");
             //const atLeast = new R().one(rule).literal("{").atLeast(1, digit).literal(",}");
             //const between = new R().one(rule).literal("{").atLeast(1, digit).literal(",").atLeast(1, digit).literal("}");
@@ -84,6 +178,8 @@ namespace Abitvin
             //const anyOfFn = (b, l) => [new Rule<TBranch, TMeta>().anyOf(b)];
             //const more = new R<TBranch, TMeta>().literal("|").anyOf(statement);
             //const anyOf = new R<TBranch, TMeta>(anyOfFn).literal("(").anyOf(statement).noneOrMany(more).literal(")");
+            
+            ranges.anyOf(atLeastOne);
             
             //const statement = new R().anyOf(all, allExcept, eof, atMost, atLeast, between, maybe, noneOrMany);
             //statement.anyOf(anyOf, atLeastOne, literal, rule);
@@ -108,10 +204,10 @@ namespace Abitvin
             
             if (rulexp == null)
             {
-                const compiled = result.branches[0];
+                const compiled = result.branches[0].rule;
                 
                 for (let i: number = 1; i < result.branches.length; i++)
-                    compiled.one(result.branches[i]);
+                    compiled.one(result.branches[i].rule);
                 
                 compiled.branchFn = branchFn;
                 compiled.meta = meta;
@@ -127,7 +223,7 @@ namespace Abitvin
                 rulexp.isDefined = true;
                 
                 for (const r of result.branches)
-                    rulexp.rule.one(r);
+                    rulexp.rule.one(r.rule);
                     
                 rulexp.rule.branchFn = branchFn;
                 rulexp.rule.meta = meta;
@@ -162,24 +258,28 @@ namespace Abitvin
     
     
     const grammer = new RulExp<number, IEmpty>();
-    
-    grammer.declare("one", "two", "three");
-    grammer.add("bla", "<one>two<two>three<three>three");
-    grammer.add("tosti", "<bla><bla>");
+    //grammer.declare("one", "two", "three");
+    //grammer.add("bla", "<one>two<two>three<three>three");
+    //grammer.add("tosti", "<bla><bla>");
     //grammer.add("anynumber", "(<one>+|<two>+|<three>+)");
-    grammer.add("one", "one", () => [1]);
-    grammer.add("two", "two", () => [2]);
-    grammer.add("three", "three", () => [3]);
+    //grammer.add("one", "one", () => [1]);
+    //grammer.add("two", "two", () => [2]);
+    //grammer.add("three", "three", () => [3]);
     //grammer.add("bla", "");
-    
+    grammer.add("foo", "foo", () => [777]);
+    grammer.add("bar", "bar", () => [888]);
+    //grammer.add("bla", "<foo>+");
+    grammer.add("bla", "<foo>+<bar>+");
     
     //console.log(grammer.scan("one", "one"));
     //console.log(grammer.scan("two", "two"));
     //console.log(grammer.scan("three", "three"));
     //console.log(grammer.scan("anynumber", "one"));
     //console.log(grammer.scan("anynumber", "twotwo"));
-    //console.log(grammer.scan("anynumber", "threethreethree"));
-    console.log(grammer.scan("tosti", "onetwotwothreethreethreeonetwotwothreethreethree"));
+    console.log(grammer.scan("bla", "foobarbarbar"));
+    console.log(grammer.scan("bla", "foofoobarbar"));
+    console.log(grammer.scan("bla", "foofoofoobar"));
+    //console.log(grammer.scan("tosti", "onetwotwothreethreethreeonetwotwothreethreethree"));
     
     const one  = new Rule<number, IEmpty>(() => [1]).literal("one");
     const two  = new Rule<number, IEmpty>(() => [2]).literal("two");
